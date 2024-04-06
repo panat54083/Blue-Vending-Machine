4d7adbdab3aa64742d15488e39d229d29b45d726
panat <panat54083@gmail.com>
2024-04-07 01:19:08 +0700

    update main function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,25 @@
 #[macro_use]
 extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/api", routes![index, delay])
+        .launch()
+        .await?;
+
+    Ok(())
 }
