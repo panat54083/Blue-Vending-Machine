0e148de0d6698e6f6c5a75373700acd3de41f95a
panat <panat54083@gmail.com>
2024-04-06 17:04:03 +0700

    install antd component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "antd";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
   );
 }
