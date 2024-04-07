901efd5e4a2098623b2ce26cf79b0bf1a3288d16
panat <panat54083@gmail.com>
2024-04-07 09:14:07 +0700

    enalble CORS

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,35 @@
 #[macro_use]
 extern crate rocket;
+
+use rocket::serde::{json::Json, Serialize};
 use rocket::tokio::time::{sleep, Duration};
+
+use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins};
+
+use std::str::FromStr;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+
+struct GOODS {
+    id: i32,
+    name: String,
+    value: i32,
+}
+
+#[get("/")]
+fn get_all_goods() -> Json<GOODS> {
+    Json(GOODS {
+        id: 1,
+        name: "test".to_string(),
+        value: 1,
+    })
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
-
 
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
@@ -16,8 +39,26 @@ async fn delay(seconds: u64) -> String {
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    let allowed_origins = AllowedOrigins::all();
+    let allowed_methods: AllowedMethods = ["Get", "Post", "Delete"]
+        .iter()
+        .map(|s| FromStr::from_str(s).unwrap())
+        .collect();
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods,
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS");
+
     let _rocket = rocket::build()
         .mount("/api", routes![index, delay])
+        .mount("/", routes![get_all_goods])
+        .attach(cors)
         .launch()
         .await?;
 
