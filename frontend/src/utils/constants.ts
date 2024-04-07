682aa5ad72199dc479382c7aefa05d983a0a8349
panat <panat54083@gmail.com>
2024-04-07 11:11:45 +0700

    change constant currency name

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,35 +2,35 @@
 export type ICurrency = {
   name: string;
   value: number; // value in THB
-  amount: number; // initial amount available
+  quantity: number; // initial amount available
 };
 
 export const COINS: ICurrency[] = [
-  { name: "OneTHB", value: 1, amount: 100 },
-  { name: "FiveTHB", value: 5, amount: 100 },
-  { name: "TenTHB", value: 10, amount: 100 },
+  { name: "OneTHB", value: 1, quantity: 100 },
+  { name: "FiveTHB", value: 5, quantity: 100 },
+  { name: "TenTHB", value: 10, quantity: 100 },
 ];
 
 export const BANKNOTES: ICurrency[] = [
-  { name: "TwentyTHB", value: 20, amount: 100 },
-  { name: "FiftyTHB", value: 50, amount: 100 },
-  { name: "HundredTHB", value: 100, amount: 100 },
-  { name: "FiveHundredTHB", value: 500, amount: 100 },
-  { name: "ThousandTHB", value: 1000, amount: 100 },
+  { name: "TwentyTHB", value: 20, quantity: 100 },
+  { name: "FiftyTHB", value: 50, quantity: 100 },
+  { name: "HundredTHB", value: 100, quantity: 100 },
+  { name: "FiveHundredTHB", value: 500, quantity: 100 },
+  { name: "ThousandTHB", value: 1000, quantity: 100 },
 ];
 
 // Define type for goods
-export type IGoods = {
+export type IProduct = {
   name: string;
   price: number; // price in THB
-  quantity: number; // initial quantity available
+  stock: number;
 };
 
 // Define constants for goods
-export const GOODS: IGoods[] = [
-  { name: "Soda", price: 20, quantity: 10 },
-  { name: "Chips", price: 30, quantity: 15 },
-  { name: "Chocolate", price: 25, quantity: 20 },
-  { name: "Water", price: 15, quantity: 12 },
-  { name: "Candy", price: 10, quantity: 25 },
+export const GOODS: IProduct[] = [
+  { name: "Soda", price: 20, stock: 10 },
+  { name: "Chips", price: 30, stock: 15 },
+  { name: "Chocolate", price: 25, stock: 20 },
+  { name: "Water", price: 15, stock: 12 },
+  { name: "Candy", price: 10, stock: 25 },
 ];
