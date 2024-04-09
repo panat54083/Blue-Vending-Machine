49c69807009e87518830dfc5517ae2d0a2db8e5c
panat <panat54083@gmail.com>
2024-04-09 22:46:28 +0700

    fix vending machine con

--- backend/src/controllers/vending-machine.controller.ts
+++ backend/src/controllers/vending-machine.controller.ts
@@ -13,142 +13,144 @@ export class VendingMachineController {
       products,
     }: { coinBanknotes: CoinBanknote[]; products: Product[] } = req.body;
 
-    // get total amount paid
-    let totalAmountPaid = 0;
-    coinBanknotes.map((coinBanknote) => {
-      totalAmountPaid += coinBanknote.value * coinBanknote.stock; // coinBanknote.stock is amount of selcted coinBanknote to be purchased
-    });
-
-    // get total price
-    let totalPrice = 0;
-    products.map((product) => {
-      totalPrice += product.price * product.stock; // product.stock is amount of selcted product to be purchased
-    });
-
-    // check if sufficient amount paid
-    if (totalAmountPaid < totalPrice) {
-      res.status(400).json({ message: "Insufficient amount paid" });
-      return;
-    }
-
-    // calculate change
-    let changeAmount = totalAmountPaid - totalPrice;
-
-    // create a new query runner
-    const queryRunner = AppDataSource.createQueryRunner();
-
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
     try {
-      // 1. update product stock in database
-      await Promise.all(
-        products.map(async (product) => {
-          const existingProduct = await this.productRepository.findOneBy({
-            id: product.id,
-          });
-
-          if (!existingProduct) {
-            res
-              .status(404)
-              .json({ message: `Product with ID ${product.id} not found` });
-            return;
-          }
-
-          if (existingProduct.stock <= 0) {
-            res.status(400).json({
-              message: `Product "${existingProduct.name}" is out of stock`,
-            });
-            return;
-          }
-
-          // product.stock is selcted product to be purchased
-          existingProduct.stock = existingProduct.stock - product.stock;
-
-          await queryRunner.manager.save(existingProduct);
-        })
-      );
-
-      // 2. return change
-      const availableCoinBanknotes = await this.coinBanknoteRepository.find();
-
-      let changeToReturn: CoinBanknote[] = [];
-
-      // Sort available coin/banknote value in descending order
-      availableCoinBanknotes.sort((a, b) => b.value - a.value);
-
-      await Promise.all(
-        availableCoinBanknotes.map(async (coinBanknote) => {
-          // Calculate number of coins/banknotes to return
-          const numberOfCoinsBanknotesToReturn = Math.min(
-            Math.floor(changeAmount / coinBanknote.value),
-            coinBanknote.stock
-          );
-
-          // Update changeToReturn
-          if (numberOfCoinsBanknotesToReturn > 0) {
-            changeToReturn.push({
-              ...coinBanknote,
-              stock: numberOfCoinsBanknotesToReturn,
-            });
-            changeAmount -= coinBanknote.value * numberOfCoinsBanknotesToReturn;
-
-            // Update coin/banknote stock
-            coinBanknote.stock -= numberOfCoinsBanknotesToReturn;
-
-            await queryRunner.manager.save(coinBanknote);
-          }
-        })
-      );
-
-      if (changeAmount < 0) {
-        res.status(400).json({
-          message: "Insufficient change to return",
-          change: changeToReturn,
-        });
-      }
-
-      if (changeAmount > 0) {
-        res.status(400).json({
-          message: "Insufficient change to return",
-          change: changeToReturn,
-        });
-      }
-
-      // 3. update coin banknotes in database
-      await Promise.all(
-        coinBanknotes.map(async (coinBanknote) => {
-          const existingCoinBanknote =
-            await this.coinBanknoteRepository.findOneBy({
-              id: coinBanknote.id,
-            });
-
-          if (!existingCoinBanknote) {
-            res
-              .status(404)
-              .json({ message: `Coin with ID ${coinBanknote.id} not found` });
-            return;
-          }
-
-          // add coin banknote which user payed to database
-          existingCoinBanknote.stock =
-            existingCoinBanknote.stock + coinBanknote.stock;
-
-          await queryRunner.manager.save(existingCoinBanknote);
-        })
-      );
-      await queryRunner.commitTransaction();
-      res.status(200).json({
-        message: "Transaction successful",
-        change: changeToReturn,
+      // get total amount paid
+      let totalAmountPaid = 0;
+      coinBanknotes.forEach((coinBanknote) => {
+        totalAmountPaid += coinBanknote.value * coinBanknote.stock; // coinBanknote.stock is amount of selected coinBanknote to be purchased
       });
-    } catch (error) {
-      console.error(error);
-      await queryRunner.rollbackTransaction();
 
-      res.status(500).json({ message: "Failed to process transaction" });
-    } finally {
-      await queryRunner.release();
+      // get total price
+      let totalPrice = 0;
+      products.forEach((product) => {
+        totalPrice += product.price * product.stock; // product.stock is amount of selected product to be purchased
+      });
+
+      // check if sufficient amount paid
+      if (totalAmountPaid < totalPrice) {
+        throw new Error("Insufficient amount paid");
+      }
+
+      // calculate change
+      let changeAmount = totalAmountPaid - totalPrice;
+
+      // create a new query runner
+      const queryRunner = AppDataSource.createQueryRunner();
+
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      try {
+        // 1. update product stock in database
+        await Promise.all(
+          products.map(async (product) => {
+            const existingProduct = await this.productRepository.findOneBy({
+              id: product.id,
+            });
+
+            if (!existingProduct) {
+              throw new Error(`Product with ID ${product.id} not found`);
+            }
+
+            if (existingProduct.stock <= 0) {
+              throw new Error(
+                `Product "${existingProduct.name}" is out of stock`
+              );
+            }
+
+            // product.stock is selected product to be purchased
+            existingProduct.stock = existingProduct.stock - product.stock;
+
+            await queryRunner.manager.save(existingProduct);
+          })
+        );
+
+        // 2. return change
+        const availableCoinBanknotes = await this.coinBanknoteRepository.find();
+
+        let changeToReturn: CoinBanknote[] = [];
+
+        // Sort available coin/banknote value in descending order
+        availableCoinBanknotes.sort((a, b) => b.value - a.value);
+
+        await Promise.all(
+          availableCoinBanknotes.map(async (coinBanknote) => {
+            // Calculate number of coins/banknotes to return
+            const numberOfCoinsBanknotesToReturn = Math.min(
+              Math.floor(changeAmount / coinBanknote.value),
+              coinBanknote.stock
+            );
+
+            // Update changeToReturn
+            if (numberOfCoinsBanknotesToReturn > 0) {
+              changeToReturn.push({
+                ...coinBanknote,
+                stock: numberOfCoinsBanknotesToReturn,
+              });
+              changeAmount -=
+                coinBanknote.value * numberOfCoinsBanknotesToReturn;
+
+              // Update coin/banknote stock
+              coinBanknote.stock -= numberOfCoinsBanknotesToReturn;
+
+              await queryRunner.manager.save(coinBanknote);
+            }
+          })
+        );
+
+        if (changeAmount > 0) {
+          throw new Error("Insufficient change to return");
+        }
+
+        // 3. update coin banknotes in database
+        await Promise.all(
+          coinBanknotes.map(async (coinBanknote) => {
+            const existingCoinBanknote =
+              await this.coinBanknoteRepository.findOneBy({
+                id: coinBanknote.id,
+              });
+
+            if (!existingCoinBanknote) {
+              throw new Error(`Coin with ID ${coinBanknote.id} not found`);
+            }
+
+            // add coin banknote which user paid to database
+            existingCoinBanknote.stock =
+              existingCoinBanknote.stock + coinBanknote.stock;
+
+            await queryRunner.manager.save(existingCoinBanknote);
+          })
+        );
+        await queryRunner.commitTransaction();
+        res.status(200).json({
+          message: "Transaction successful",
+          change: changeToReturn,
+        });
+      } catch (error: any) {
+        console.error(error);
+        await queryRunner.rollbackTransaction();
+
+        if (error.message === "Insufficient change to return") {
+          res.status(400).json({
+            message: error.message,
+          });
+        } else {
+          res.status(500).json({ message: "Failed to process transaction" });
+        }
+      } finally {
+        await queryRunner.release();
+      }
+    } catch (error: any) {
+      console.error(error);
+      if (
+        error.message === "Insufficient amount paid" ||
+        error.message.startsWith("Product with ID") ||
+        error.message.startsWith('Product "')
+      ) {
+        res.status(400).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: "Failed to process transaction" });
+      }
     }
   }
 }
