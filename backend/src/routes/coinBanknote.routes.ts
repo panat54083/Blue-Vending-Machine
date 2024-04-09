7f7c13d2b47a3b5979d1f0c98a0f9b7ec79691dd
panat <panat54083@gmail.com>
2024-04-09 11:45:39 +0700

    update coin-banknote api name

--- backend/src/routes/coinBanknote.routes.ts
+++ backend/src/routes/coinBanknote.routes.ts
@@ -5,27 +5,27 @@ const router = express.Router();
 const coinBanknoteController = new CoinBanknoteController();
 
 // GET /coin-banknotes
-router.get("/coin-banknotes", async (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
   await coinBanknoteController.getAllCoinBanknotes(req, res);
 });
 
 // GET /coin-banknotes/:id
-router.get("/coin-banknotes/:id", async (req: Request, res: Response) => {
+router.get("/:id", async (req: Request, res: Response) => {
   await coinBanknoteController.getCoinBanknoteById(req, res);
 });
 
 // POST /coin-banknotes
-router.post("/coin-banknotes", async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
   await coinBanknoteController.createCoinBanknote(req, res);
 });
 
 // PUT /coin-banknotes/:id
-router.put("/coin-banknotes/:id", async (req: Request, res: Response) => {
+router.put("/:id", async (req: Request, res: Response) => {
   await coinBanknoteController.updateCoinBanknote(req, res);
 });
 
 // DELETE /coin-banknotes/:id
-router.delete("/coin-banknotes/:id", async (req: Request, res: Response) => {
+router.delete("/:id", async (req: Request, res: Response) => {
   await coinBanknoteController.deleteCoinBanknote(req, res);
 });
 
